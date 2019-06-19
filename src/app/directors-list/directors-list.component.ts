9ba9684eafd524b332cd5eb68e04f083a6cce482
GimYoungPhil <youngphil.gim@gmail.com>
2019-06-19 17:12:15 +0900

    fixed directors-list

--- src/app/directors-list/directors-list.component.ts
+++ src/app/directors-list/directors-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Director } from '../director';
+import { DirectorService } from '../director.service';
+
 @Component({
   selector: 'app-directors-list',
   templateUrl: './directors-list.component.html',
   styleUrls: ['./directors-list.component.scss']
 })
 export class DirectorsListComponent implements OnInit {
+  directors: Director[];
 
-  constructor() { }
+  constructor(private directorService: DirectorService) { }
 
   ngOnInit() {
+    this.getDirectors();
+  }
+
+  getDirectors(): void {
+    this.directorService.getDirectors()
+        .subscribe(directors => this.directors = directors);
   }
 
 }
