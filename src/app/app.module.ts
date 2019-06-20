6738b2574f2d4a7c674bf68bd3aae8dda8ff31f4
GimYoungPhil <youngphil.gim@gmail.com>
2019-06-20 09:56:40 +0900

    add MessageService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { TopbarComponent } from './topbar/topbar.component';
 import { DirectorsListComponent } from './directors-list/directors-list.component';
 import { DirectorsDetailComponent } from './directors-detail/directors-detail.component';
 import { AboutComponent } from './about/about.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +17,11 @@ import { AboutComponent } from './about/about.component';
     DirectorsListComponent,
     DirectorsDetailComponent,
     AboutComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
--- src/app/director.service.ts
+++ src/app/director.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
 import { Director } from './director';
 import { DIRECTORS } from './mock-directors';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DirectorService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) { }
 
   getDirectors(): Observable<Director[]> {
+    this.messageService.add('DirectsService: fetched directors');
     return of(DIRECTORS);
   }
 }
